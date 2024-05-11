f48031cd9ed33506d68bfd3063b05f6fb1c3b39b
sayedkhaledd <sayedkhaledsa@gmail.com>
2024-05-12 01:16:48 +0300

    init

---
+++ src/main/java/com/example/paymentservice/service/PaymentService.java
@@ -0,0 +1,25 @@
+package com.example.paymentservice.service;
+
+import com.example.paymentservice.grpc.PaymentRequest;
+import com.example.paymentservice.grpc.PaymentResponse;
+import com.example.paymentservice.grpc.PaymentServiceGrpc;
+import io.grpc.stub.StreamObserver;
+import lombok.AllArgsConstructor;
+import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@GrpcService
+public class PaymentService extends PaymentServiceGrpc.PaymentServiceImplBase {
+
+    @Override
+    public void processPayment(PaymentRequest request, StreamObserver<PaymentResponse> responseObserver) {
+        PaymentResponse response = PaymentResponse.newBuilder()
+                .setSuccess(true)
+                .setTransactionId("txn_" + System.currentTimeMillis())
+                .build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+
+    }
+}
